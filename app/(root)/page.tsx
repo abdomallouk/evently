97c1bd94b5a165c0d6d483260ab258d1acc7cc01
abdomallouk <abdelbassetmallouk@gmail.com>
2025-01-10 11:32:16 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
@@ -28,6 +36,30 @@ export default async function Home() {
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
         </div>
+      </section>
+
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
+        </h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          {/* <Search /> */}
+          {/* <CategoryFilter /> */}
+        </div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          // totalPages={events?.totalPages}
+          totalPages={2}
+        />
       </section>
     </>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["utfs.io", "res.cloudinary.com"], // Add res.cloudinary.com here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
